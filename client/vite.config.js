f1d2318e2d90b48e3969307c579ed5db8d5e3489
shikha1522 <agarwalvanshika1515@gmail.com>
2026-09-10 21:54:02 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import env from 'dotenv';
-env.config();
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': `${import.meta.env.VITE_API_URL}`
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true
+        }
+      }
     }
   }
 })
